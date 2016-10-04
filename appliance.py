d72ea65b1e42a65d24588ffce6351b3c7951e609
Devon Coleman <dccoleman@wpi.edu>
2016-10-04 16:07:49 -0400

    dicks

--- appliance.py
+++ appliance.py
@@ -1,56 +1,89 @@
-from netfilterqueue import NetfilterQueue
-from dnslib import DNSRecord
+import subprocess
+import datetime
+import Queue
+import time
+import threading
 
-import socket
+ttl = 2 #time to live in seconds
+honeypot = "10.44.12.1" #the honeypot's address
+server = "10.44.12.2" 
+prefix = "10.44.12."
 
-from scapy.all import *
+mapped = Queue.Queue()
+unmapped = Queue.Queue()
 
-from constants import *
-from map_honeypot import *
-from map_server import map_server
+class slot:
+	def __init__(self, interface, ip_addr):
+		temptimeout = datetime.datetime.now()
+		self.timeout = (temptimeout + datetime.timedelta(0,ttl))
+		self.interface = interface		
+		self.ip = ip_addr
+		
+	def setTimeout(self):
+		temptimeout = datetime.datetime.now()
+		self.timeout = (temptimeout + datetime.timedelta(0,ttl))
 
-from slot import slot
+def setup(num):
+	subprocess.call(["echo", "1", ">", "/proc/sys/net/ipv4/ip_forward"])
 
-def acceptAll(pkt) :
-	d = IP(pkt.get_payload())
+	for i in xrange(0,num):
+		test = slot(i,i+50)
+		map_honeypot(test)
+		subprocess.call(["sudo", "ifconfig", "enp0s3:" + str(i), "10.44.13." + str(i + 50)])
 
-	print "'" + d['DNS Question Record'].qname + "'"
-	print d[IP].src
+def map_honeypot(bucket):
+		subprocess.call(["sudo", "iptables", "-t", "nat", "-A", "PREROUTING", "-d", prefix + str(bucket.ip), "-j", "DNAT", "--to-destination", honeypot]) #map to honeypot
+		subprocess.call(["sudo", "iptables", "-t", "nat", "-A", "POSTROUTING", "-s", honeypot, "-j", "SNAT", "--to-source", prefix + str(bucket.ip)]) #map honeypot back to source IP (could conflict with multiple mappings?)
+		unmapped.put(bucket)
 
-	if(d['DNS Question Record'].qname != "cap.com."):
-		pkt.accept()
-	else:	
-		print "Found cap.com!"
-		print d.summary()
-		print d[IP].src
+def map_server():
+	if(unmapped.empty()):
+		return None
+	else:
+		bucket = unmapped.get()
+		subprocess.call(["sudo", "iptables", "-t", "nat", "-A", "PREROUTING", "-d", prefix + str(bucket.ip), "-j", "DNAT", "--to-destination", honeypot]) #map to honeypot
+		subprocess.call(["sudo", "iptables", "-t", "nat", "-A", "POSTROUTING", "-s", server, "-j", "SNAT", "--to-source", prefix + str(bucket.ip)])
+		bucket.setTimeout()
+		print "mapped " + prefix + str(bucket.ip)
+		mapped.put(bucket)
+		return bucket
 
-		#modify dns zone file
+def timeout_mappings():
+	print "starting thread"
+	while True:
+		if(mapped.empty()):
+			#print "nothing"
+			time.sleep(.2)
+			continue
+		else:
+			currtime = datetime.datetime.now()
+			if(currtime >= mapped.queue[0].timeout):
+				obj2 = mapped.get()
+				map_honeypot(obj2)
+				#unmap from server
+				print "unmapped " + prefix + str(obj2.ip)
+				continue
 
-		if(map_server(d[IP].src) == None):
-			pkt.reject()
-			#drop connection, all slots used up
+			diff = (mapped.queue[0].timeout - currtime).seconds
+			if(diff >= .1):
+				print "waiting " + str(diff)
+				time.sleep((mapped.queue[0].timeout - currtime).seconds)
+				continue
+	print "ending thread"
 
 
-		#print ' '.join(c.encode('hex') for c in pkt.get_payload()[0:20])
-		pkt.accept()
+#script
 
-subprocess.call(["iptables", "-t", "nat", "-F"])
-for i in xrange(0,slots):
-	mapping = slot(i,i+50,"10.4.12.1")
-	subprocess.call(["ifconfig", iface + ":" + str(i), prefix + str(i + 50)])
-	map_honeypot(mapping)
+setup(3)
 
-subprocess.call(["iptables", "-t", "nat", "-I", "POSTROUTING", "-o", iface, "-j", "MASQUERADE"])
-subprocess.call(["ifconfig", iface + ":255", prefix + "255"])
-subprocess.call(["iptables", "-I", "INPUT", "-d", prefix+"255", "-j", "NFQUEUE", "--queue-num", "1"])
+t = threading.Thread(target = timeout_mappings)
+t.daemon = True
+t.start()
 
-nfqueue = NetfilterQueue()
-nfqueue.bind(1, acceptAll)
-s = socket.fromfd(nfqueue.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
-try:
-    nfqueue.run_socket(s)
-except KeyboardInterrupt:
-    print('')
+map_server()
+time.sleep(5)
+map_server()
+map_server()
 
-s.close()
-nfqueue.unbind()
+time.sleep(999999999)
+
