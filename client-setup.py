f36cf9ed42b99fd9d9f55f231aa77733e43e77e8
Kyle McCormick <kylemccor@gmail.com>
2016-10-09 11:40:53 -0700

    Finish testing

---
+++ client-setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# CS 4404 final project
+# Devon Coleman, Kyle McCormick, Christopher Navarro, William Van Rensselaer
+
+import subprocess
+
+from shared_constants import *
+
+def main():
+    for i in range(0, NUM_CLIENTS):
+        addr = ADDR_PREFIX + `START_CLIENT_IP + i`
+        subprocess.call(["ifconfig", "ens33:"+`i`, addr, "netmask", "255.255.255.192"])
+
+main()
+
--- client.py
+++ client.py
@@ -9,39 +9,51 @@ import time
 from shared_constants import *
 
 CLIENT_COUNTS = [1, 5, 15, 50, 100]
-START_IP_NUMBER = 150
+TRIALS_PER_COUNT = 10
 
 response_count = None
 response_times = None
 
 def thread_runner(addr):
-    print "Sending request from " + addr
-    APP_ADDR = 8.8.8.8
-    proc = subprocess.Popen(["dig", "-b", addr, "@" + APP_ADDR, "www.cap.com"])
+    global response_count
+    global response_times
+
+    APP_ADDR = "8.8.8.8"
+    proc = subprocess.Popen(["dig", "-b", addr, "@" + APP_ADDR, "www.cap.com"], stdout=subprocess.PIPE)
     proc.wait()
     output = proc.stdout.read()
-    i = output.index("Query time: ")
+    try:
+        i = output.index("Query time: ")
+    except ValueError:
+        response_times[addr] = None
+        return
     ln = len("Query time: ")
     time_str = output[i+ln:i+ln+10].split(' ')[0]
-    response_times[addr] = int(time_str) / 1000.0
+    secs = int(time_str) / 1000.0
+    response_times[addr] = secs 
     response_count += 1
-    print "Received response for " + addr + " in " + elapsed + " s"
 
 def main(): 
     global response_count
     global response_times
 
     for client_count in CLIENT_COUNTS:
+        print "\n"
         print "Testing with " + `client_count` + " clients"
-        response_count = 0
-        response_times = {}
-        for i in range(0, client_count):
-            addr = ADDR_PREFIX + `START_IP_NUMBER + i`
-            threading.Thread(target=thread_runner, args=(addr,)).start()
-        while response_count < client_count:
-            time.sleep(0.1)
-        summed_times = sum(time for _, time in response_times.iteritems())
-        avg = summed_times
-        print "Average time for " + `client_counts " clients: " + `avg` + " s"
+        average_sum = 0
+        for trial in range(0, TRIALS_PER_COUNT):
+            print "  Trial #" + `trial`
+            response_count = 0
+            response_times = {}
+            for i in range(0, client_count):
+                addr = ADDR_PREFIX + `START_CLIENT_IP + i`
+                threading.Thread(target=thread_runner, args=(addr,)).start()
+            while response_count < client_count:
+                time.sleep(0.1)
+            times = [t for _, t in response_times.iteritems() if t is not None]
+            avg = sum(times) / len(times)
+            print "    Average response time: " + `avg` + " s"
+            average_sum += avg
+        print "  Overall average response time: " + `average_sum / TRIALS_PER_COUNT` + " s"
 
 main()
